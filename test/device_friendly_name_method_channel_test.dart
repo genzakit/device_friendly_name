359632764b0b008fc9063b0992faed3973ebf945
Nhat Tran <nhat.tv16@gmail.com>
2023-09-04 16:24:22 +0700

    Fixbug

--- test/device_friendly_name_method_channel_test.dart
+++ test/device_friendly_name_method_channel_test.dart
@@ -1,22 +1,10 @@
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:device_friendly_name/device_friendly_name_method_channel.dart';
 
 void main() {
   MethodChannelDeviceFriendlyName platform = MethodChannelDeviceFriendlyName();
-  const MethodChannel channel = MethodChannel('device_friendly_name');
 
   TestWidgetsFlutterBinding.ensureInitialized();
-
-  setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
-    });
-  });
-
-  tearDown(() {
-    channel.setMockMethodCallHandler(null);
-  });
 
   test('getPlatformVersion', () async {
     expect(await platform.getPlatformVersion(), '42');
